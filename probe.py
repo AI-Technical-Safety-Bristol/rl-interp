316a76334c3abf9fde44a9014ab6dca65f13b510
Aidan Ewart <aidanprattewart@gmail.com>
2023-03-24 01:13:57 +0000

    visuals

--- probe.py
+++ probe.py
@@ -3,7 +3,9 @@ from torch import nn
 import torch.nn.functional as F
 from goofy import MultiLayerFeedForward
 
+from matplotlib.widgets import Slider
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 class ProbedFeedForward(nn.Module):
     def __init__(self, n_layers, n_inputs, d_model, n_outputs):
@@ -37,12 +39,11 @@ class ProbedFeedForward(nn.Module):
         pff.out_layer.bias = net.out_layer.bias
         return pff
 
-def show_act(inp, probe, out):
+def show_act(inp, probe, out, fig, ax):
     """
     Show the activations stored in probe, with output in out, on the plot plt
     The activations should be plotted as a 2D black and white image
     """
-    fig, ax = plt.subplots(3, 1)
     fig.suptitle("activations")
 
     ax[0].imshow(inp.unsqueeze(0).detach().numpy(), cmap="gray")
@@ -58,40 +59,78 @@ def show_act(inp, probe, out):
     ax[2].set_xlabel("neuron")
     ax[2].set_yticks([])
 
-def show_weights(net):
+def show_weights(net, fig, ax):
     """
     Show the weights of the network net
     The weights shoud be presented as a 2D black and white image per layer
     All images should be shown on the same figure
     Each image should have axis titles indicating to and from neurons
     """
-    fig, ax = plt.subplots(1, len(net.layers)+2)
     fig.suptitle("weights")
 
     ax[0].imshow(net.in_layer.weight.detach().numpy(), cmap="gray")
     ax[0].set_xlabel("from")
     ax[0].set_ylabel("to")
-    ax[0].set_title("input layer")
+    ax[0].set_title("input proj")
 
     for i, l in enumerate(net.layers):
         ax[i+1].imshow(l.weight.detach().numpy(), cmap="gray")
         ax[i+1].set_xlabel("from")
         ax[i+1].set_ylabel("to")
-        ax[i+1].set_title(f"layer {i}")
+        ax[i+1].set_title(f"layer {i} proj")
 
     ax[-1].imshow(net.out_layer.weight.transpose(0, 1).detach().numpy(), cmap="gray")
     ax[-1].set_xlabel("to")
     ax[-1].set_ylabel("from")
-    ax[-1].set_title("output layer")
+    ax[-1].set_title("output proj")
 
 model = MultiLayerFeedForward(2, 8, 4, 4)
-model.load_state_dict(torch.load("policy_10K.pt"))
+model.load_state_dict(torch.load("policy_100K.pt"))
 model = ProbedFeedForward.from_mlff(model)
 
-inp = torch.rand(8)
+inp = torch.zeros(8)
 out = model(inp)
 
-show_act(inp, model.probe, out)
-show_weights(model)
+weight_fig, weight_ax = plt.subplots(1, len(model.layers)+2)
+
+show_weights(model, weight_fig, weight_ax)
+
+act_fig = plt.figure()
+gs = GridSpec(6, 2)
+
+ac0 = act_fig.add_subplot(gs[0, 1])
+ac1 = act_fig.add_subplot(gs[1:5, 1])
+ac2 = act_fig.add_subplot(gs[5, 1])
+
+act_ax = [ac0, ac1, ac2]
+
+show_act(inp, model.probe, out, act_fig, act_ax)
+
+slider_ax = [act_fig.add_subplot(gs[i, 0]) for i in range(6)]
+
+# 8 sliders to set the input values
+# high values: [1.5, 1.5, 5., 5., 3.14, 5., 1., 1. ]
+# low values: [-1.5, -1.5, -5., -5., -3.14, -5., -0., -0. ]
+
+x_coord = Slider(slider_ax[0], 'x', -1.5, 1.5, valinit=0.0)
+y_coord = Slider(slider_ax[1], 'y', -1.5, 1.5, valinit=0.0)
+x_vel = Slider(slider_ax[2], 'x_vel', -5., 5., valinit=0.0)
+y_vel = Slider(slider_ax[3], 'y_vel', -5., 5., valinit=0.0)
+angle = Slider(slider_ax[4], 'angle', -3.14, 3.14, valinit=0.0)
+angle_vel = Slider(slider_ax[5], 'angle_vel', -5., 5., valinit=0.0)
+#left_leg = Slider(act_ax[-2], 'left_leg', -1., 1., valinit=0.0)
+#right_leg = Slider(act_ax[-1], 'right_leg', -1., 1., valinit=0.0)
+
+def update(val, fig, ax):
+    inp = torch.tensor([x_coord.val, y_coord.val, x_vel.val, y_vel.val, angle.val, angle_vel.val, 0, 0], dtype=torch.float32)
+    out = model(inp)
+    show_act(inp, model.probe, out, fig, ax)
+
+x_coord.on_changed(lambda val: update(val, act_fig, act_ax))
+y_coord.on_changed(lambda val: update(val, act_fig, act_ax))
+x_vel.on_changed(lambda val: update(val, act_fig, act_ax))
+y_vel.on_changed(lambda val: update(val, act_fig, act_ax))
+angle.on_changed(lambda val: update(val, act_fig, act_ax))
+angle_vel.on_changed(lambda val: update(val, act_fig, act_ax))
 
 plt.show()
